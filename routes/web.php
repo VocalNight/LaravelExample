00ff937ef6d43bf81788025d312d8822e8664b2a
Vocal <vocalnight@protonmail.com>
2024-03-27 11:38:04 -0300

    Day 7, learned about classes/models and the abort helper function

--- routes/web.php
+++ routes/web.php
@@ -1,60 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
+
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function ()  {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,00'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,00'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$20,00'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50,00'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10,00'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$20,00'
-        ]
-        ];
-    
-     //   Arr::first($jobs, function ($job) use ($id) {
-     //       return $job['id'] == $id;
-     //   });
-
-     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-    return view('job', ['job' => $job]);
+    return view('job', ['job' => Job::find($id)]);
 });
 
 Route::get('/contact', function () {
