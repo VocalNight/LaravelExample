ffde4e674043aee12db811dae13fc8dd1b34ad48
Vocal <vocalnight@protonmail.com>
2024-04-24 21:39:47 -0300

    day 10, revisited somethings

--- database/migrations/2024_03_28_034229_create_employers_table.php
+++ database/migrations/2024_03_28_034229_create_employers_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listings', function (Blueprint $table) {
+        Schema::create('employers', function (Blueprint $table) {
             $table->id();
-            //foreign id
-            //One of the options for foreign key instead of the foreingId
-            //$table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(App\Models\Employer::class);
-            $table->string('title');
-            $table->string('salary');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listings');
+        Schema::dropIfExists('employers');
     }
 };
