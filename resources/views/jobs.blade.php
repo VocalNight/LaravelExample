20ba0c90c0897b179dcbc24ea66a7f3879df449a
Vocal <vocalnight@protonmail.com>
2024-04-29 15:47:45 -0300

    day 13, lazy loading and eager loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -3,13 +3,14 @@
         Job Page
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <a href="/jobs/{{$job['id']}}" class="text-gray-500 hover:underline">
-                <li>
-                    <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}}
-                </li>
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">{{ $job->employer->name}}</div>
+            <div>
+                <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}}
+            </div>
             </a>
         @endforeach
-    </ul>
+        </div>
 </x-layout>
