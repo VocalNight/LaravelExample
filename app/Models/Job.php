dc834bd8b51e2e24a715e31e8fdb6fbe428ad6e7
Vocal <vocalnight@protonmail.com>
2024-04-29 22:15:52 -0300

    day 16, forms, posts and Cross site protection

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -10,6 +10,11 @@ class Job extends Model {
     use HasFactory;
     protected $table = 'job_listings';
 
+    protected $fillable = ['employer_id', 'title', 'salary'];
+
+    //disable the fillable protection
+    //protected $guarded = [];
+
     public function employer() {
 
         return $this->belongsTo(Employer::class);
