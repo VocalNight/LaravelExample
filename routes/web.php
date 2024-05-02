ef0e201c4dcc5994a9e35eaafacd5649bc38a3a1
Vocal <vocalnight@protonmail.com>
2024-05-02 01:55:51 -0300

    day 19, fixed typo

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function() {
-    
-// Index
-Route::get('/jobs', 'index');
-
-// Store
-Route::post('/jobs', 'store');
-
-//Create
-Route::get('/jobs/create', 'create');
-
-// Show
-Route::get('/jobs/{job}', 'show');
-
-// Edit
-Route::get('/jobs/{job}/edit', 'edit');
-
-// Update
-Route::patch('/jobs/{job}', 'update');
-
-// Destroy
-Route::delete('/jobs/{job}', 'destroy');
-});
-
-
+Route::resource('jobs', JobController::class);
 
 Route::view('/contact', 'contact');
